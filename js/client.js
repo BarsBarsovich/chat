711cc308cd46dc208ef4aef3d032843da31360d4
barspredator <boris.dryabin@gmail.com>
2019-03-24 23:58:35 +0300

    Чат

--- js/client.js
+++ js/client.js
@@ -26,9 +26,22 @@ const sendMessageBtn = document.getElementById('sendMessage');
 
 sendMessageBtn.addEventListener('click', () => {
 
-    const data = {type: 'message', data: {message: document.getElementById('message').value, id: document.getElementById('currentUserId').innerText}};
+    const data = {
+        type: 'message', data: {
+            message: document.getElementById('message').value,
+            id: document.getElementById('currentUserId').innerText
+        }
+    };
     ws.send(JSON.stringify(data));
 });
+
+const loadImageBtn = document.getElementById('loadImage');
+
+loadImageBtn.addEventListener('click', ()=>{
+    document.getElementById('dropZone').style.display= 'block';
+    document.getElementById('dropZone').style.opacity= 1;
+})
+
 
 
 ws.onmessage = function (event) {
@@ -38,10 +51,20 @@ ws.onmessage = function (event) {
     switch (data.type) {
         case 'allUsers':
             const users = data.data.userOnServer;
+            const posts = data.post;
             for (const user of users) {
 
-                if (currentUserFio == user.data.nick){
+                if (currentUserFio == user.data.nick) {
                     document.getElementById('currentUserId').innerText = user.id;
+                    const postsArea = document.querySelector('.message__list');
+                    if (posts !== undefined && posts.allPosts !== undefined) {
+                        for (let post of posts.allPosts) {
+                            console.log('getPOsts');
+                            const userPost = document.createElement('div');
+                            userPost.innerText = post.message;
+                            postsArea.appendChild(userPost);
+                        }
+                    }
                 }
                 const name = user.data.nick;
                 const userElement = document.createElement('div');
@@ -56,16 +79,76 @@ ws.onmessage = function (event) {
             document.querySelector('.users__list').appendChild(userElement);
             break;
         case 'newMessage':
-            console.log('condition works');
+            console.log(data);
+            const mList = document.querySelector('.message__list')
+            const newElement = document.createElement('div')
+            newElement.innerText = data.messageText;
+            mList.appendChild(newElement);
+            break;
+        case 'userLogout':
             break;
     }
 }
 
 ws.onclose = function (event) {
-    console.log('Server was closed');
+    data = {
+        type: 'clientClose',
+        id: document.getElementById('currentUserId').innerText
+
+    }
+    ws.send(JSON.stringify(data))
 }
 
 
 ws.onerror = function (err) {
     console.log(err);
+}
+
+const dropzoneLabel = document.querySelector('.load-img__dropzone-label');
+const loadImgButton = document.querySelectorAll('.load-img__button');
+
+
+    dropzoneLabel.addEventListener('drop', e => {
+        let dt = e.dataTransfer
+        let file = dt.files
+
+        previewImage(file[0]).then(base64image => {
+            // сохраняем картинку из промиса для того чтобы функция в обработчике могла её использовать
+            _base64image = base64image;
+            console.log(_base64image)
+            loadImgButton[1].classList.remove('load-img__button--inactive');
+            loadImgButton[1].addEventListener('click', sendImageToServerWrapper);
+        });
+
+        return false;
+    });
+
+function previewImage(file) {
+    return new Promise((resolve, reject) => {
+        if (file.size <= 512 * 1024) {
+            // errorAlert.style.opacity = 0;
+            renderImage(file).then(base64image => resolve(base64image));
+        } else {
+            // errorAlert.style.opacity = 1;
+            reject();
+        }
+    })
+}
+
+// рендеринг изображения
+function renderImage(file) {
+    return new Promise((resolve) => {
+        let base64image = '';
+        var reader = new FileReader();
+
+        reader.onload = function (event) {
+            base64image = event.target.result;
+
+            dropzoneLabel.style.backgroundImage = `url(${base64image})`;
+            dropzoneLabel.innerText = ''
+
+            resolve(base64image)
+        }
+        reader.readAsDataURL(file)
+    })
 }
--- js/server.js
+++ js/server.js
@@ -1,4 +1,5 @@
 const WebSocket = require('ws');
+const fs = require('fs');
 const uuidv1 = require('uuid/v1');
 
 const wss = new WebSocket.Server({port: 5501});
@@ -11,7 +12,7 @@ const FileSync = require('lowdb/adapters/FileSync')
 
 const adapter = new FileSync('db.json')
 const db = low(adapter)
-db.defaults({posts: [], users: []}).write();
+db.defaults({posts: [], users: [], avatars:[]}).write();
 
 wss.on('connection', function (ws) {
     console.log('connect sever');
@@ -24,11 +25,40 @@ wss.on('connection', function (ws) {
         switch (message.type) {
             case 'login':
                 const userTable = db.get('users');
+                const userOnServer = userTable.value();
 
-                // первое что нужно сделать отдать всех пользователей, кто сейчас на серваке. Включая текущего
-                db.get('users').push({id: id, data: {fio: message.data.name, nick: message.data.nick}}).write();
+                // если у нас такой пользователь уже был, то его надо сделать активным и отдать все его данные
+                let previousId;
 
-                const userOnServer = db.get('users').value();
+                for (let item of userOnServer){
+                    if (item.data.nick === message.data.nick) {
+                        console.log('condition done');
+                        previousId =  item.id;
+                    }
+                }
+
+                if (previousId) {
+                    userTable.find({id: previousId}).assign({status: 'active'}).write();
+                    const allPosts = db.get('posts').value();
+
+                    for (let i = 0; i < allPosts.length; i++){
+                        if (allPosts[i].userId !== previousId){
+                            allPosts.splice(i,1);
+                        }
+                    }
+                    ws.send(JSON.stringify({type: 'allUsers', data: {userOnServer}, post: {allPosts}}));
+                    return;
+                } else {
+                    // первое что нужно сделать отдать всех пользователей, кто сейчас на серваке. Включая текущего
+                    db.get('users').push({id: id, data: {fio: message.data.name, nick: message.data.nick}}).write();
+                }
+
+                // из нашаго списка удалим неактивных
+                for (let i = 0; i < userOnServer.length; i++) {
+                    if (userOnServer[i].status === 'unactive') {
+                        userOnServer.splice(i, 1);
+                    }
+                }
 
                 ws.send(JSON.stringify({type: 'allUsers', data: {userOnServer}}));
 
@@ -52,19 +82,28 @@ wss.on('connection', function (ws) {
                 const messageText = message.data.message;
                 const userId = message.data.id;
 
-                db.get('posts').push({ userId: userId, message: messageText}).write();
+                db.get('posts').push({userId: userId, message: messageText}).write();
                 // console.log(messagide)
 
+                for (const key in clients) {
+                    clients[key].send(JSON.stringify({type: 'newMessage', messageText: messageText}))
+                }
+
                 // получаем сообщение и отправляем его всем, включая текущего
-                ws.send(JSON.stringify({type: 'newMessage', messageText: messageText}));
+                // ws.send(JSON.stringify({type: 'newMessage', messageText: messageText}));
                 break;
             case 'avatar':
+                console.log(message);
+                fs.writeFile(`./uploads/1.png`, message.data);
+                clients[id].send(JSON.stringify({type: 'upload', data:{}}))
                 break;
         }
     });
 
     ws.on('close', function () {
         delete clients[id];
+        // в нашей таблице надо сделать пользователя неактивным и обновить всех клиентов
+        db.get('users').find({id: id}).assign({status: 'unactive'}).write();
     });
 });
 
